524f4050d6ade536fa1e5d5f4dabc75594abccad
asapovk <asapovk@gmail.com>
2024-02-11 17:56:07 +0300

    upd light controller

--- packages/bite-light-controller-v1/lib/index.ts
+++ packages/bite-light-controller-v1/lib/index.ts
@@ -5,15 +5,15 @@ import { DefautOpts, InitArgsType, MakeBiteReducerType, TriggerPhaseKeys, WatchA
 export function biteLightController<Tg, St, K extends keyof Tg, RTg>(biteName: K, props: {
     reducer?: MakeBiteReducerType<Tg, RTg,St, K>,
     script: {
-        init: (opt: DefautOpts<Tg, St, K, Tg>, initPayload: unknown) => Promise<void> | void;
-        watchScope?: Array<Partial<Record<keyof RTg, TriggerPhaseKeys<RTg, keyof RTg> | Array<TriggerPhaseKeys<RTg, keyof RTg>>>> | keyof RTg> | Array<keyof RTg>;
-        watch?: (opt: DefautOpts<Tg, St, K, Tg>, watchArgs: WatchArgsType<Tg, K>) => Promise<void> | void;
+        init: (opt: DefautOpts<Tg, St, K, RTg>, initPayload: unknown) => Promise<void> | void;
+        watchScope?: Array<keyof RTg>;
+        watch?: (opt: DefautOpts<Tg, St, K, RTg>, watchArgs: WatchArgsType<Tg, K>) => Promise<void> | void;
     }
 }) {
   return Bite<Tg, St, K, RTg>(
     props.reducer || null as any, 
     {
-        watchScope: props.script.watchScope || [biteName as any],
+        watchScope: props.script.watchScope as any || [biteName as any],
         instance: 'stable',
         script: LightControllerScript,
         initOn: 'init' as any,
