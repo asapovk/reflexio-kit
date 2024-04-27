fa330f686ed526cba0a197a6e9ce1d5c2f90d898
asapovk <asapovk@gmail.com>
2024-04-27 17:20:02 +0300

    add bite-atom

---
+++ packages/bite-atom-v1/lib/index.ts
@@ -0,0 +1,37 @@
+import { Bite } from '@reflexio/core-v1';
+import { MakeBiteReducerType, UpdateOnType } from '@reflexio/core-v1/lib/types';
+
+export function biteAtom<Tg, St, K extends keyof Tg, RTg>(
+  biteName: K,
+  props: {
+    api: MakeBiteReducerType<Tg, RTg, St, K> extends {init: any, dropt: any} ? MakeBiteReducerType<Tg, RTg, St, K>: never;
+    watchScope?: UpdateOnType<RTg>;
+    script?: any;
+    instance?: 'stable' | 'refreshing' | 'multiple';
+    initialState?: any;
+  }
+) {
+  return Bite<Tg, St, K, RTg>(
+    {
+        ...props.api, 
+        init: (state, payload) => {
+            state[biteName] = props.initialState; 
+        },
+        drop: (state, payload) => {
+            state[biteName] = null;
+        },
+        setState: (state, payload) => {
+            state[biteName] = payload
+        },
+        mergeToState: (state, payload) => {
+            Object.assign(state[biteName], payload);
+        }
+    } as any,
+    {
+      watchScope: props.watchScope || [biteName as any],
+      instance: props.instance || 'stable',
+      script: props.script,
+      initOn: 'init' as any,
+    }
+  );
+}
