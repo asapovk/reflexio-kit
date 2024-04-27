d32091a7eb7bd26962db4a1a18ca360597a873f6
asapovk <asapovk@gmail.com>
2024-04-28 02:49:44 +0300

    fix typing

--- packages/bite-atom-v1/lib/types.ts
+++ packages/bite-atom-v1/lib/types.ts
@@ -2,9 +2,9 @@ import { BiteStatusWrap } from '@reflexio/core-v1/lib/types';
 
 
 
-export type IAtomTriggersApi<ASt, ATg> = BiteStatusWrap< ATg & {
-    init: null;
-    drop: null;
+export type IAtomTriggersApi<ASt, ATg> = BiteStatusWrap< {
+    init: 'init' extends keyof ATg ? ATg['init'] : null;
+    drop: 'drop' extends keyof ATg ? ATg['drop'] : null;
     setState: ASt;
-    mergeToState: Partial<ASt>
-}>
+    mergeToState: Partial<ASt>;
+} & ATg>
